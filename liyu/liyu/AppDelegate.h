ddc5ca123ff75b034be8128ced1b23de2e751c7e
XiaoYiS <yihaiyang1203@sina.com>
2015-11-21 08:46:43 +0800

    创建项目
    11

---
+++ liyu/liyu/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  liyu
+//
+//  Created by apple-jd12 on 15/11/21.
+//  Copyright © 2015年 hazel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ liyu/liyu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  liyu
+//
+//  Created by apple-jd12 on 15/11/21.
+//  Copyright © 2015年 hazel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
